ebe3057b0e259fd79a30e5c0ff5b50fd6c442f5d
ceilingfang <romannequin@gmail.com>
2022-08-08 14:10:56 -0700

    added js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Your Code Here
 let userName = window.prompt(RomanNavarro)
+for(let whatIsTheBookOfTheOldTestament = 0; whatIsTheBookOfTheOldTestament < questions.length; whatIsTheBookOfTheOldTestament++) {
+
+}
