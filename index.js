615a46e5f57dc829fdb21ad66efed3bf86e6e663
ceilingfang <romannequin@gmail.com>
2022-08-08 14:51:42 -0700

    added js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Your Code Here
 let userName = window.prompt('RomanNavarro')
 for(let correctAnswer = 0; correctAnswer < questions.length; correctAnswer++) {
-
+    console.log(questions.correctAnswer)
 }
-console.log(questions.correctAnswer)
