ff3a63a81cbb6942df337f23e36dd76bffd1172c
ceilingfang <romannequin@gmail.com>
2022-08-08 15:14:52 -0700

    fixed js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your Code Here
-let userName = window.prompt('RomanNavarro')
+let userName = window.prompt('Please Enter Your Name')
 for(let correctAnswer = 0; correctAnswer < questions.length; correctAnswer++) {
     console.log(questions.correctAnswer)
 }
