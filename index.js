89a35e7b99604748fe14fd42ba6bd7af91168dde
ceilingfang <romannequin@gmail.com>
2022-08-08 20:51:46 -0700

    added js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your Code Here
 let userName = window.prompt('Please Enter Your Name')
 for(let i = 0; i < questions.length; i++) {
-    console.log(questions.text)
+    console.log(questions[i].text)
 }
